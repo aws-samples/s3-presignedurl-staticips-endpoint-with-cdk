afd85ba994e8dc1809c8bbea14e073bad7361472
Jin, Song <sonjin@amazon.com>
2023-11-22 23:30:54 +0900

    Initial commit

---
+++ test/unified-s3-endpoint.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UnifiedS3Endpoint from '../lib/unified-s3-endpoint-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/unified-s3-endpoint-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UnifiedS3Endpoint.UnifiedS3EndpointStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
