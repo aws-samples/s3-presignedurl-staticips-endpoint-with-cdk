fc23c9bd33219c7d4abfcd6d0f4f7cc56ed4bf5d
Jin, Song <sonjin@amazon.com>
2023-11-22 23:48:32 +0900

    Add GA, VPC, ALB

--- bin/unified-s3-endpoint.ts
+++ bin/unified-s3-endpoint.ts
@@ -15,7 +15,7 @@ new UnifiedS3EndpointStack(app, 'UnifiedS3EndpointStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '535106968734', region: 'ap-northeast-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/unified-s3-endpoint-stack.ts
+++ lib/unified-s3-endpoint-stack.ts
@@ -1,16 +1,45 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as globalaccelerator from 'aws-cdk-lib/aws-globalaccelerator';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ga_endpoints from 'aws-cdk-lib/aws-globalaccelerator-endpoints';
 
 export class UnifiedS3EndpointStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create an Accelerator
+    const accelerator = new globalaccelerator.Accelerator(this, 'Accelerator');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'UnifiedS3EndpointQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a Listener
+    const listener = accelerator.addListener('Listener', {
+      portRanges: [
+        { fromPort: 80 },
+        { fromPort: 443 },
+      ],
+    });
+
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
+    });
+
+    // Create the load balancer in a VPC. 'internetFacing' is 'false'
+    // by default, which creates an internal load balancer.
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc
+    });
+
+    // Add one EndpointGroup for each Region we are targeting
+    listener.addEndpointGroup('Group', {
+      endpoints: [
+        new ga_endpoints.ApplicationLoadBalancerEndpoint(alb, {
+          weight: 128,
+          preserveClientIp: true,
+        }),
+      ],
+    });
+
+
   }
 }
